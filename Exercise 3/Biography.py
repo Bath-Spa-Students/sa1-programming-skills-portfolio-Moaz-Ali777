561389474f957e318b992a74b7b82d92aa4e6471
Moaz-Ali777 <muaazali2535@gmail.com>
2024-11-22 00:47:47 +0400

    Create Biography.py
    this code shows how dictionary can be used to display information

---
+++ Exercise 3/Biography.py
@@ -0,0 +1,18 @@
+'''
+## Exercise 3: Biography - 25 Marks
+
+In this exercise, you'll create a program that stores and prints your name, hometown, and age to the console using a Python dictionary.
+
+### Steps:
+1. Store the information (name, hometown, and age) as key-value pairs in a dictionary.
+2. Print the values on separate lines using a single print() statement.
+3. Use variables with appropriate data types for each piece of information.
+'''
+
+personaldetails = {
+    "name": "Moaz",
+    "hometown": "Al  Ghubaiba",
+    "age": 16
+}
+
+print(f"Name: {personaldetails['name']}\nHometown: {personaldetails['hometown']}\nAge: {personaldetails['age']}")
