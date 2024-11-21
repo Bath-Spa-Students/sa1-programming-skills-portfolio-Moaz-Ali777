533748d389947c70e919d5429124982913acc604
Moaz-Ali777 <muaazali2535@gmail.com>
2024-11-22 00:57:45 +0400

    Create Primitive quiz advance requirments.py
    this shows the more advance way of using the if else statement and how helpful it is to answer according the situation.

---
+++ Exercise 4/Primitive quiz advance requirments.py
@@ -0,0 +1,66 @@
+'''
+### Advanced Requirements:
+#Ignore Capitalization: Modify your program to accept answers regardless of the capitalization (e.g., "paris", "Paris", and "PaRis" should all be considered correct).
+#Multiple Questions: Extend the program into a quiz that asks for the capitals of 10 European countries. Provide feedback for each question.
+'''
+
+country = input("WHAT IS THE CAPITAL OF FRANCE? ")
+if country.lower() == "Paris":
+    print("your answer is correct!")
+else:
+    print("your answer is wrong.")
+
+
+country = input("WHAT IS THE CAPITAL IF ENGLAND? ")
+if country.lower() == "London":
+    print("your answer is correct!")
+else:
+    print("your answer is wrong.")
+
+country = input("WHAT IS THE CAPITAL OF GERMANY? ")
+if country.lower() == "Berlin":
+    print("ypur answer is correct!")
+else:
+    print("your answer is wrong.")
+
+country = input("WHAT IS THE CAPITAL OF ITALY? ")
+if country.lower() == "Rome":
+    print("your answer is correct!")
+else:
+    print("your answer is wrong.")
+
+country = input("WHAT IS THE CAPITAL OF GREECE? ")
+if country.lower() == "Athens":
+    print("your answer is correct!")
+else:
+    print("your answer is wrong.")  
+
+country = input("WHAT IS THE CAPITAL OF SPAIN? ")
+if country.lower() == "Madrid":
+    print("your answer is correct!")
+else:
+    print("your answer is wrong.")
+
+country = input("WHAT IS THE CAPITAL OF IRELAND? ")
+if country.lower() == "Dublin":
+    print("your answer is correct!")
+else:
+    print("your answer is wrong.")
+
+country = input("WHAT IS THE CAPITAL OF RUSSIA? ")
+if country.lower() == "Moscow":
+    print("your answer is correct!")
+else:
+    print("your answer is wrong.")
+
+country = input("WHAT IS THE CAPITAL OF BULGARIA? ")
+if country.lower() == "Sofia":
+    print("your answer is correct!")
+else:
+    print("your answer is wrong.")
+
+country = input("WHAT IS THE CAPITAL OF NORWAY? ")
+if country.lower() == "Olso":
+    print("your answer is correct!")
+else:
+    print("your answer is wrong.")
